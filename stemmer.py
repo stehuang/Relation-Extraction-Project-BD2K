f27de5d6e28eacc63d42c9d7c06fe9a7e07f9ad6
Stella <stellahyhuang@hotmail.com>
2019-05-21 16:01:34 -0700

    extracts all proteins in corpus

--- stemmer.py
+++ stemmer.py
@@ -9,9 +9,6 @@ import sys
 test_file = "./iepa_bioc.xml"
 BIOC_OUT = 'iepa_output.xml'
 
-# write proteins to file
-f = open("relations.txt","x")
-
 
 reader = BioCXMLReader(test_file)
 bioc_writer = BioCXMLWriter(BIOC_OUT)
@@ -20,19 +17,33 @@ bioc_writer.collection = reader.collection
 
 stemmer = PorterStemmer()
 
+# file to save all text
+#f = open("passages.txt", "w")
+g = open("proteins.txt", "w")
+
 # Get documents to manipulate
 documents = bioc_writer.collection.documents
     
 # Go through each document
 annotation_id = 0
-for document in documents[0:0]:
+for document in documents:
         
     # Go through each passage of the document
     for passage in document:
-        #  Stem all the tokens found
-        stems = [stemmer.stem(token) for token in wordpunct_tokenize(passage.text)]
-            # Add an anotation showing the stemmed version, in the given order
-        for stem in stems:
+
+    	# create dictionary that maps protein id to protein name
+    	text = dict()
+    	for annotation in passage.annotations:
+            text[annotation.id] = annotation.text	
+
+        # Stem all the tokens found
+    	stems = [stemmer.stem(token) for token in wordpunct_tokenize(passage.text)]
+
+        #save passage
+        #f.write(passage.text + '\n')
+
+        # Add an anotation showing the stemmed version, in the given order
+    	for stem in stems:
             annotation_id += 1
                 # For each token an annotation is created, providing
                 # the surface form of a 'stemmed token'.
@@ -44,24 +55,20 @@ for document in documents[0:0]:
             bioc_annotation.put_infon('surface form', 'stemmed token')
             passage.add_annotation(bioc_annotation)
 
+        # extract all proteins present in corpus
+    	for relation in passage.relations:
+        	for node in relation.nodes:
+        		if(node.refid in text.keys()):
+        			g.write(text[node.refid] + '\n')
 
+#f.close()
+g.close()
+print(text)
 # Print file to screen w/o trailing newline
 # (Can be redirected into a file, e. g output_bioc.xml)
-sys.stdout.write(str(bioc_writer))
+#sys.stdout.write(str(bioc_writer))
     
 # Write to disk
 bioc_writer.write()
-
-
-
-
-
-
-
-
-
-
-# with open(test_file, 'r') as fp:
-#     collection = bioc.load(fp)
 
 
