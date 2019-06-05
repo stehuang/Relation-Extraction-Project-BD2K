6ceba73b9cd00d636d1a29fdba983a07c382bae0
Stella <stellahyhuang@hotmail.com>
2019-06-04 17:35:11 -0700

    get indices of proteins

---
+++ pipeline_verb.py
@@ -0,0 +1,81 @@
+import nltk, imp
+from nltk.tokenize import wordpunct_tokenize
+from nltk import PorterStemmer
+from itertools import combinations 
+
+# import list of proteins
+proteins = []
+g = open("proteins.txt", "r")
+for line in g:
+	proteins.append(line.rstrip('\n'))
+proteins = set(proteins)
+
+f = open("passages.txt", "r")
+#results = open("interactions.txt", "w")
+#h = open("co_relations_verbs.txt", "x")
+
+
+with open('passages.txt') as myFile:
+  text = myFile.read()
+corpus = text.split('!')
+
+
+# create dict of interactions
+interactions = dict()
+count = 1
+document = 1
+
+# process passage by passage
+for passage in corpus:
+	#results.write('\n' + 'Passage ' + str(document))
+	#h.write('\n' + '//' + '\n')
+	document += 1
+	#results.write(passage + '\n')
+	mod_passage = passage.split('\n')
+	for line in mod_passage:
+		# tokenize sentence
+		curr_proteins = []
+		protein_index = []
+		sentence = wordpunct_tokenize(line)
+		# create list of proteins in sentence
+		for token in proteins:
+	 		if token in line:
+	 			curr_proteins.append(token)
+		redundant = []
+	 	# list interactions by creating every possible pair of 2 proteins
+	 	# save as tuple in dictionary
+	 	# key: relation #, value: tuple of proteins
+
+	 	# remove proteins that possibly don't exist
+		for i in range(len(curr_proteins)):
+	 		for j in range(len(curr_proteins)):
+	 			if(curr_proteins[i]) in curr_proteins[j] and curr_proteins[i] != curr_proteins[j]:
+	 				redundant.append(curr_proteins[i])
+
+		curr_proteins = [x for x in curr_proteins if x not in redundant]
+		curr_proteins = sorted(curr_proteins) #arrange tuple in alphabetical order
+
+		# get the index of proteins in the sentence
+		protein_index = dict()
+		for protein in curr_proteins:
+			protein_index[protein] = line.find(protein)
+
+		print(protein_index)
+		# for i in range(len(curr_proteins)):
+	 # 		for j in range(len(curr_proteins)):
+		pairs = list(combinations(curr_proteins, 2))
+
+		for relation in pairs:
+			relation = tuple(relation)
+			interactions['relation'+str(count)] = relation
+			#results.write('relation ' + str(count) +': ' + str(relation) + '\n')
+			#h.write(str(relation) + '\n')
+			count += 1
+			
+
+#results.close()
+
+
+
+
+
